fa8a44b0bbbfe21bd8549685093e3fe7fa6f17f9
Domino <zhikhanastasya@gmail.com>
2018-07-01 00:19:24 +0300

    feat (Logic.Task2) Newton's method for searching of root and basic tests

---
+++ Logic.Task2.Tests/AdditionalMathPackTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Logic.Task2.Tests
+{
+    [TestFixture]
+    class AdditionalMathPackTests
+    {
+        [TestCase(1, 3, 0.0001, ExpectedResult = 1)]
+        [TestCase(8, 3, 0.0001, ExpectedResult = 2)]
+        [TestCase(0.001, 3, 0.0001, ExpectedResult = 0.1)]
+        [TestCase(0.04100625, 4, 0.0001, ExpectedResult = 0.45)] //
+        [TestCase(8, 3, 0.0001, ExpectedResult = 2)]
+        [TestCase(0.0279936, 7, 0.0001, ExpectedResult = 0.6)] //
+        [TestCase(0.0081, 4, 0.1, ExpectedResult = 0.3)] //
+        [TestCase(-0.008, 3, 0.1, ExpectedResult = -0.2)]
+        [TestCase(0.004241979, 9, 0.00000001, ExpectedResult = 0.545)] //
+        public double FindNthRoot_Integer_Tree_FourSing(double value, int degree, double eps)
+        {
+           return value.FindNthRoot(degree, eps);
+        }
+        
+        [Test]
+        public void FindNthRoot_NegativeValue_PositiveDegree_PositivePrecision_ArgumentOutOfRangeException()
+        {
+            double value = -0.01;
+
+            Assert.Throws<ArgumentException>(() => value.FindNthRoot(2, 0.0001));
+        }
+
+        [Test]
+        public void FindNthRoot_PositiveValue_NegativeDegree_PositivePrecision_ArgumentOutOfRangeException()
+        {
+            double value = 0.01;
+
+            Assert.Throws<ArgumentException>(() => value.FindNthRoot(-2, 0.0001));
+        }
+
+        [Test]
+        public void FindNthRoot_PositiveValue_PositiveDegree_NegativePrecision_ArgumentOutOfRangeException()
+        {
+            double value = 0.01;
+
+            Assert.Throws<ArgumentException>(() => value.FindNthRoot(2, -0.0001));
+        }
+    }
+}
---
+++ Logic.Task2/AdditionalMathPack.cs
@@ -0,0 +1,140 @@
+﻿using System;
+
+namespace Logic.Task2
+{
+    /// <summary>
+    /// This class provides extensions for double.
+    /// </summary>
+    public static class AdditionalMathPack
+    {
+        #region Constants
+        private const double DefaultValue = 0;
+        private const double ZeroRoot = 1;
+        #endregion
+
+        #region Public methods (extensions for double)
+        /// <summary>
+        /// This method finds n-root with getting precision.
+        /// </summary>
+        /// <param name="rootDegree"> Degree </param>
+        /// <param name="eps"> Precision </param>
+        /// <returns> N-root of value </returns>
+        /// <exception cref="ArgumentException"> If rootDegree is less than zero </exception>
+        /// <exception cref="ArgumentException"> If eps is less than zero </exception>
+        /// <exception cref="ArgumentException"> If value is less than zero and rootDegree is odd number </exception>
+        public static double FindNthRoot(this double value, int rootDegree, double eps)
+        {
+            if (rootDegree < 0)
+            {
+                throw new ArgumentException("Argument degree can't be less than 0!");
+            }
+
+            if (eps < 0)
+            {
+                throw new ArgumentException("Argument eps can't be less than 0!");
+            }
+
+            if ((value < 0) && (rootDegree % 2 == 0))
+            {
+                throw new ArgumentException("It is impossible to extract odd degree root of a negative number!");
+            }
+
+            if ((value == DefaultValue) || (value == 1))
+            {
+                return value;
+            }
+
+            if (rootDegree == 0)
+            {
+                return ZeroRoot;
+            }
+
+            if (rootDegree == 1)
+            {
+                return value;
+            }
+
+            return FindNewtonZeros(value, rootDegree, eps);
+        }
+
+        /// <summary>
+        /// This method round value with getting precision.
+        /// </summary>
+        /// <param name="eps"> Precision </param>
+        /// <returns> Rounded value </returns>
+        /// <exception cref="ArgumentException"> If eps is less than zero </exception>
+        public static double Round(this double value, double eps)
+        {
+            if (eps < 0)
+            {
+                throw new ArgumentException("Argument eps can't be less than 0!");
+            }
+
+            int accuracy = GetAccuracy(eps);
+
+            return Math.Round(value, accuracy);
+        }
+        #endregion
+
+        #region Private methods
+        /// <summary>
+        /// This method calculates Newton's expression for finding root.
+        /// </summary>
+        /// <param name="value"> Value for calculation </param>
+        /// <param name="rootDegree"> Degree of root </param>
+        /// <param name="previousValue"> Previous mean of value </param>
+        /// <returns> New value getting by Newton's expression </returns>
+        private static double CalcNewtonExpression(double value, int rootDegree, double previousValue)
+        {
+            double result = (rootDegree - 1) * previousValue;
+
+            result += value / Math.Pow(previousValue, rootDegree - 1);
+
+            result /= rootDegree;
+
+            return result;
+        }
+
+        /// <summary>
+        /// This method finds root using Newton's method.
+        /// </summary>
+        /// <param name="value"> Value for calculation </param>
+        /// <param name="rootDegree"> Degree of root </param>
+        /// <param name="eps">  Precision </param>
+        /// <returns> N-root of value</returns>
+        private static double FindNewtonZeros(double value, int rootDegree, double eps)
+        {
+            double comparingValue = value / rootDegree;
+            double result = CalcNewtonExpression(value, rootDegree, comparingValue);
+
+            while (Math.Abs(result - comparingValue) > eps)
+            {
+                comparingValue = result;
+
+                result = CalcNewtonExpression(value, rootDegree, comparingValue);
+            }
+
+            return result.Round(eps);
+        }
+
+        /// <summary>
+        /// This method finds number of digits.
+        /// </summary>
+        /// <param name="eps">  Precision </param>
+        /// <returns> Number of digits </returns>
+        private static int GetAccuracy(double eps)
+        {
+            int result = 0;
+
+            while (eps != 1)
+            {
+                eps *= 10;
+
+                result++;
+            }
+
+            return result;
+        }
+        #endregion
+    }
+}
