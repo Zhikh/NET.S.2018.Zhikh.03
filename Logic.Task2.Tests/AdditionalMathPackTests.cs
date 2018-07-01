3e64f6809c76412ddb2b87f09c49ac7edc9033f7
Domino <zhikhanastasya@gmail.com>
2018-07-01 19:19:59 +0300

    chore (Logic.Task2.Tests) include tests for ArgumentException in one

--- Logic.Task2.Tests/AdditionalMathPackTests.cs
+++ Logic.Task2.Tests/AdditionalMathPackTests.cs
@@ -9,39 +9,19 @@ namespace Logic.Task2.Tests
         [TestCase(1, 3, 0.0001, ExpectedResult = 1)]
         [TestCase(8, 3, 0.0001, ExpectedResult = 2)]
         [TestCase(0.001, 3, 0.0001, ExpectedResult = 0.1)]
-        [TestCase(0.04100625, 4, 0.0001, ExpectedResult = 0.45)] //
+        [TestCase(0.04100625, 4, 0.0001, ExpectedResult = 0.45)]
         [TestCase(8, 3, 0.0001, ExpectedResult = 2)]
-        [TestCase(0.0279936, 7, 0.0001, ExpectedResult = 0.6)] //
-        [TestCase(0.0081, 4, 0.1, ExpectedResult = 0.3)] //
+        [TestCase(0.0279936, 7, 0.0001, ExpectedResult = 0.6)]
+        [TestCase(0.0081, 4, 0.1, ExpectedResult = 0.3)]
         [TestCase(-0.008, 3, 0.1, ExpectedResult = -0.2)]
-        [TestCase(0.004241979, 9, 0.00000001, ExpectedResult = 0.545)] //
-        public double FindNthRoot_Integer_Tree_FourSing(double value, int degree, double eps)
-        {
-           return value.FindNthRoot(degree, eps);
-        }
-        
-        [Test]
-        public void FindNthRoot_NegativeValue_PositiveDegree_PositivePrecision_ArgumentOutOfRangeException()
-        {
-            double value = -0.01;
+        [TestCase(0.004241979, 9, 0.00000001, ExpectedResult = 0.545)]
+        public double FindNthRoot_Integer_Tree_FourSing(double value, int degree, double eps) 
+            => value.FindNthRoot(degree, eps);
 
-            Assert.Throws<ArgumentException>(() => value.FindNthRoot(2, 0.0001));
-        }
-
-        [Test]
-        public void FindNthRoot_PositiveValue_NegativeDegree_PositivePrecision_ArgumentOutOfRangeException()
-        {
-            double value = 0.01;
-
-            Assert.Throws<ArgumentException>(() => value.FindNthRoot(-2, 0.0001));
-        }
-
-        [Test]
-        public void FindNthRoot_PositiveValue_PositiveDegree_NegativePrecision_ArgumentOutOfRangeException()
-        {
-            double value = 0.01;
-
-            Assert.Throws<ArgumentException>(() => value.FindNthRoot(2, -0.0001));
-        }
+        [TestCase(-0.01, 2, 0.0001)]
+        [TestCase(0.01, -2, 0.0001)]
+        [TestCase(0.01, 2, -0.0001)]
+        public void FindNthRoot_Value_Degree_Precision_ArgumentException(double value, int degree, double eps) 
+            => Assert.Throws<ArgumentException>(() => value.FindNthRoot(degree, eps));
     }
 }
