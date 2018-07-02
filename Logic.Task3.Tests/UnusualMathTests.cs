891101b30ede17de6cc30a4820895a37944798e5
Domino <zhikhanastasya@gmail.com>
2018-07-02 18:12:39 +0300

    add (Logic.Task3.Tests) basic tests

---
+++ Logic.Task3.Tests/UnusualMathTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Logic.Task3.Tests
+{
+    [TestFixture]
+    public class UnusualMathTests
+    {
+        [TestCase(12, ExpectedResult = 21)]
+        [TestCase(513, ExpectedResult = 531)]
+        [TestCase(2017, ExpectedResult = 2071)]
+        [TestCase(414, ExpectedResult = 441)]
+        [TestCase(144, ExpectedResult = 414)]
+        [TestCase(1234321, ExpectedResult = 1241233)]
+        [TestCase(1234126, ExpectedResult = 1234162)]
+        [TestCase(3456432, ExpectedResult = 3462345)]
+        [TestCase(10, ExpectedResult = -1)]
+        [TestCase(20, ExpectedResult = -1)]
+        [TestCase(0, ExpectedResult = -1)]
+        public int FindNextBiggerNumber_Integer_CorrectResult(int value)
+        {
+            int actual = 0;
+
+            UnusualMath.FindNextBiggerNumber(value, ref actual);
+
+            return actual;
+        }
+
+        [TestCase(-231)]
+        public void FindNextBiggerNumber_Integer_ArgumentException(int value)
+        {
+            int actual = 0;
+
+            Assert.Throws<ArgumentException>(() => UnusualMath.FindNextBiggerNumber(value, ref actual));
+        }
+    }
+}
