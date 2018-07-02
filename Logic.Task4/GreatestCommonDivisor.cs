f3573c1c7d71cd1902f0fa39b5577323dcc62343
Domino <zhikhanastasya@gmail.com>
2018-07-02 20:55:25 +0300

    change (Logic.Task4)

--- Logic.Task4/GreatestCommonDivisor.cs
+++ Logic.Task4/GreatestCommonDivisor.cs
@@ -4,6 +4,9 @@ namespace Logic.Task4
 {
     public static class GreatestCommonDivisor
     {
+        public delegate int FindGCDAlgorithm(int x, int y);
+        #region Euclidean
+
         public static int FindGCDEuclidean(int firstValue, int secondValue)
         {
             int temp;
@@ -18,6 +21,7 @@ namespace Logic.Task4
             return Math.Abs(secondValue);
         }
 
+        #region дублирующийся код
         public static int FindGCDEuclidean(int firstValue, int secondValue, int thirdValue)
         {
             int gcd = FindGCDEuclidean(firstValue, secondValue);
@@ -43,5 +47,42 @@ namespace Logic.Task4
 
             return gcd;
         }
+        #endregion
+        #endregion
+
+        #region Stein
+        public static int FindGCDStein(int firstValue, int secondValue)
+        {
+            
+        }
+        #endregion
+
+        #region дублирующийся код
+        public static int FindGCDStein(int firstValue, int secondValue, int thirdValue, FindGCDAlgorithm callback)
+        {
+            int gcd = callback(firstValue, secondValue);
+
+            return callback(gcd, thirdValue);
+        }
+
+        public static int FindGCDStein(int firstValue, int secondValue, int thirdValue, int fourthValue, FindGCDAlgorithm callback)
+        {
+            int gcd = FindGCDStein(firstValue, secondValue, thirdValue, callback);
+
+            return callback(gcd, fourthValue);
+        }
+
+        public static int FindGCDStein(FindGCDAlgorithm callback, params int[] values)
+        {
+            int gcd = 0;
+
+            foreach (var value in values)
+            {
+                gcd = callback(gcd, value);
+            }
+
+            return gcd;
+        }
+        #endregion
     }
 }
