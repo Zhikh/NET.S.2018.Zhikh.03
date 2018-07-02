d512d545c65a1860f4764c57d8392c3403ef2559
Domino <zhikhanastasya@gmail.com>
2018-07-02 20:06:28 +0300

    add (Logic.Task4) methods for finding gcd by Euclidean's algorithm

---
+++ Logic.Task4/GreatestCommonDivisor.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Logic.Task4
+{
+    public static class GreatestCommonDivisor
+    {
+        public static int FindGCDEuclidean(int firstValue, int secondValue)
+        {
+            int temp;
+
+            while (firstValue != 0)
+            {
+                temp = firstValue;
+                firstValue = secondValue % firstValue;
+                secondValue = temp;
+            }
+
+            return Math.Abs(secondValue);
+        }
+
+        public static int FindGCDEuclidean(int firstValue, int secondValue, int thirdValue)
+        {
+            int gcd = FindGCDEuclidean(firstValue, secondValue);
+
+            return FindGCDEuclidean(gcd, thirdValue);
+        }
+
+        public static int FindGCDEuclidean(int firstValue, int secondValue, int thirdValue, int fourthValue)
+        {
+            int gcd = FindGCDEuclidean(firstValue, secondValue, thirdValue);
+
+            return FindGCDEuclidean(gcd, fourthValue);
+        }
+
+        public static int FindGCDEuclidean(params int[] values)
+        {
+            int gcd = 0;
+
+            foreach (var value in values)
+            {
+                gcd = FindGCDEuclidean(gcd, value);
+            }
+
+            return gcd;
+        }
+    }
+}
