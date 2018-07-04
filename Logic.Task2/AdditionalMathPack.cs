9a630b4e9e1abcd29eaeae631bc58a4cb1f6e2d5
Domino <zhikhanastasya@gmail.com>
2018-07-04 21:18:34 +0300

    refactor (Logic.Task2)

--- Logic.Task2/AdditionalMathPack.cs
+++ Logic.Task2/AdditionalMathPack.cs
@@ -29,7 +29,7 @@ namespace Logic.Task2
                 throw new ArgumentException("Argument degree can't be less than 0!");
             }
 
-            if (eps < 0)
+            if ((eps > 0) || (eps < 1))
             {
                 throw new ArgumentException("Argument eps can't be less than 0!");
             }
@@ -56,52 +56,21 @@ namespace Logic.Task2
 
             return FindNewtonZeros(value, rootDegree, eps);
         }
-
-        /// <summary>
-        /// This method round value with getting precision.
-        /// </summary>
-        /// <param name="eps"> Precision </param>
-        /// <returns> Rounded value </returns>
-        /// <exception cref="ArgumentException"> If eps is less than zero </exception>
-        public static double Round(this double value, double eps)
-        {
-            if (eps < 0)
-            {
-                throw new ArgumentException("Argument eps can't be less than 0!");
-            }
-
-            int accuracy = GetAccuracy(eps);
-
-            return Math.Round(value, accuracy);
-        }
         #endregion
 
         #region Private methods
-        /// <summary>
-        /// This method calculates Newton's expression for finding root.
-        /// </summary>
-        /// <param name="value"> Value for calculation </param>
-        /// <param name="rootDegree"> Degree of root </param>
-        /// <param name="previousValue"> Previous mean of value </param>
-        /// <returns> New value getting by Newton's expression </returns>
+        // This method calculates Newton's expression for finding root.
         private static double CalcNewtonExpression(double value, int rootDegree, double previousValue)
         {
             double result = (rootDegree - 1) * previousValue;
 
             result += value / Math.Pow(previousValue, rootDegree - 1);
-
             result /= rootDegree;
 
             return result;
         }
-
-        /// <summary>
-        /// This method finds root using Newton's method.
-        /// </summary>
-        /// <param name="value"> Value for calculation </param>
-        /// <param name="rootDegree"> Degree of root </param>
-        /// <param name="eps">  Precision </param>
-        /// <returns> N-root of value</returns>
+        
+        // This method finds root using Newton's method.
         private static double FindNewtonZeros(double value, int rootDegree, double eps)
         {
             double comparingValue = value / rootDegree;
@@ -110,29 +79,9 @@ namespace Logic.Task2
             while (Math.Abs(result - comparingValue) > eps)
             {
                 comparingValue = result;
-
                 result = CalcNewtonExpression(value, rootDegree, comparingValue);
             }
-
-            return result.Round(eps);
-        }
-
-        /// <summary>
-        /// This method finds number of digits.
-        /// </summary>
-        /// <param name="eps">  Precision </param>
-        /// <returns> Number of digits </returns>
-        private static int GetAccuracy(double eps)
-        {
-            int result = 0;
-
-            while (eps != 1)
-            {
-                eps *= 10;
-
-                result++;
-            }
-
+            
             return result;
         }
         #endregion
